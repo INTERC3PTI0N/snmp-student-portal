43178285ab5ae4ad04e66e60eec6acc1c86c3853
Umar HB <52497733+INTERC3PTI0N@users.noreply.github.com>
2024-02-23 19:41:41 +0530

    Update fire.js
    removed deprecated API

--- js/fire.js
+++ js/fire.js
@@ -1,11 +1,10 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBxogVMrdgMoibTqBGkYO9x9gSuSFg456E",
-    authDomain: "snmp-3eab1.firebaseapp.com",
-    projectId: "snmp-3eab1",
-    storageBucket: "snmp-3eab1.appspot.com",
-    messagingSenderId: "581968628905",
-    appId: "1:581968628905:web:dcc1900e993fc004086db8",
-    measurementId: "G-7M1ECEJRG6"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "m",
+    messagingSenderId: "",
+    appId: ""
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
